68f72aa35f9f676505a713e63a7aea92b5956684
Kevin Washington <87237986+KevinWashington@users.noreply.github.com>
2024-09-06 20:34:40 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.152.2';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 // Cena, câmera e renderizador
 const scene = new THREE.Scene();
@@ -7,8 +8,31 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const controls = new OrbitControls( camera, renderer.domElement );
+
+//controls.update() must be called after any manual changes to the camera's transform
+camera.position.set( 0, 0, 100 );
+controls.update();
+
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	// required if controls.enableDamping or controls.autoRotate are set to true
+	controls.update();
+
+	renderer.render( scene, camera );
+
+}
+
+animate()
+
+
+
+
 // Criando geometria de um plano (BufferGeometry)
-const geometry = new THREE.PlaneGeometry(10, 10, 32, 32);  // 32 segmentos para suavidade
+const geometry = new THREE.PlaneGeometry(10, 20, 32, 64);  // 32 segmentos para suavidade
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 const plane = new THREE.Mesh(geometry, material);
 scene.add(plane);
@@ -19,10 +43,28 @@ const vertex = new THREE.Vector3();
 
 // Aplicando uma curva senoidal aos vértices
 for (let i = 0; i < positionAttribute.count; i++) {
+  
   vertex.fromBufferAttribute(positionAttribute, i);
-  const curveAmount = Math.sin(vertex.x * Math.PI / 5);  // Função senoidal
-  vertex.z = curveAmount;  // Modificando o eixo Z
-  positionAttribute.setXYZ(i, vertex.x, vertex.y, vertex.z);
+  
+  if(i <= positionAttribute.count/3+10){
+    
+    let curveAmount = 4;  
+    vertex.z = curveAmount;
+    // Modificando o eixo Z
+    positionAttribute.setXYZ(i, vertex.x, vertex.y-3, vertex.z);
+  }else if(i >= positionAttribute.count/3*2-11){
+   
+    let curveAmount = 4
+    vertex.z = curveAmount;
+    positionAttribute.setXYZ(i, vertex.x, vertex.y+3, vertex.z)
+  }else{
+    
+    let curveAmount = 1
+    vertex.z = curveAmount;
+    positionAttribute.setXYZ(i, vertex.x, vertex.y, vertex.z)
+  }
+    
+  
 }
 
 // Atualizando a geometria
@@ -31,9 +73,4 @@ positionAttribute.needsUpdate = true;
 // Posição da câmera
 camera.position.z = 15;
 
-// Função de render
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-}
-animate();
+
